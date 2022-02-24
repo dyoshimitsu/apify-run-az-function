bff245709436a460329c8163fa41b61d13dfcfe3
dyoshimitsu <d.yoshimitsu@gmail.com>
2022-02-24 21:58:15 +0900

    add apify

--- function/HttpTrigger/index.ts
+++ function/HttpTrigger/index.ts
@@ -1,15 +1,26 @@
-import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import { AzureFunction, Context } from '@azure/functions'
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  context.log('HTTP trigger function processed a request.')
-  const name = (req.query.name || (req.body && req.body.name))
-  const responseMessage = name
-    ? 'Hello, ' + name + '. This HTTP triggered function executed successfully.'
-    : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.'
+const httpTrigger: AzureFunction = async function (context: Context): Promise<void> {
+  const Apify = require('apify')
+  const requestList = await Apify.openRequestList('urls', [{ url: 'https://news.google.com/' }])
+  const handlePageFunction = async ({ page, _ }) => {
+    await page.pdf({ path: 'example.pdf' })
+  }
+
+  const crawler = new Apify.PuppeteerCrawler({
+    requestList,
+    handlePageFunction,
+    launchContext: {
+      launchOptions: {
+        headless: true
+      }
+    }
+  })
+
+  await crawler.run()
 
   context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage
+    status: 200
   }
 }
 
